eb3bf7adce9c2469ca00626be957f8999744f7b1
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2021-01-25 11:11:27 +0000

    Inital commit

---
+++ EmeraldBank/src/com/github/awesomekalin/emeraldbank/Main.java
@@ -0,0 +1,13 @@
+package com.github.awesomekalin.emeraldbank;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	@Override
+	public void onDisable() {
+		
+	}
+}
